ae947f3bf32fc300f6e6e54b6d2ae8b8b2207812
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-12-13 15:00:09 +0700

    fix: bug show turn

--- src/components/GameControl/index.tsx
+++ src/components/GameControl/index.tsx
@@ -1,8 +1,9 @@
 'use client';
 import Button from '@/components/Button';
 import { XIcon, OIcon, ReloadIcon } from '@/components/Icons';
-
+import { useGameContext } from '@/providers/GameProvider';
 export default function GameControl() {
+  const { isXNext } = useGameContext();
   return (
     <div className="flex justify-between items-center w-full max-w-[460px] mb-6">
       <div className="flex gap-2">
@@ -10,9 +11,9 @@ export default function GameControl() {
         <OIcon className="text-yellow-400" />
       </div>
 
-      <Button color="navy">
+      <Button color="navy" className="hover:!bg-navy-400 cursor-auto">
         <div className="flex items-center justify-center gap-2">
-          <XIcon className="w-5 h-5" />
+          {isXNext ? <XIcon className="w-5 h-5" /> : <OIcon className="w-5 h-5" />}
           TURN
         </div>
       </Button>
